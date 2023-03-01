1cc26b6607b631a7e79e660269b3011f95687bf3
SalmonBoyFred <michaelfdow025@gmail.com>
2023-03-01 14:30:54 -0500

    work on displaying game pieces in individual squares

--- tictactoe.js
+++ tictactoe.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.container');
+let gamePiece = document.createElement('p');
 
 // Module for Game Board
 
@@ -25,11 +26,48 @@ const gameFlow = (() => {
         playerTwo,
         displayMoves : function () {
             for (const playerMove of gameBoard.board) {
-                const gamePiece = document.createElement('p');
+                
                 gamePiece.classList.add('gamePiece');
                 gamePiece.textContent = playerMove;
                 container.appendChild(gamePiece);
+                console.log(playerMove);
+                
             }
-        }
+        },
     }
 })();
+
+const boxes = document.querySelectorAll('div.box');
+
+boxes.forEach((div) => {
+    div.addEventListener('click', (e) => {
+        gameFlow.displayMoves();
+        e.target.appendChild(gamePiece);
+    });
+});
+
+
+
+
+
+
+
+/*
+boxes.forEach((div) => {
+    div.addEventListener('click', () => {
+        gameFlow.displayMoves();
+        this.appendChild(gamePiece);
+    });
+});
+
+displayMoves : function () {
+    for (const playerMove of gameBoard.board) {
+        const gamePiece = document.createElement('p');
+        gamePiece.classList.add('gamePiece');
+        gamePiece.textContent = playerMove;
+        return gamePiece;
+        // container.appendChild(gamePiece); 
+    }
+}
+
+*/
